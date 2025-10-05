eca312616407b1575c36edeaabc9cd4710dea42e
monokkai <egormayer08@mail.ru>
2025-10-05 23:12:12 +0200

    ♻️refactor: configuring the prettier and eslint

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
+  console.log(req.method);
+  console.log("-----------");
   res.send(200, "Hello, from /!");
 });
 
