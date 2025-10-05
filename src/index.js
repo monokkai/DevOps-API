ccf18f2917277345404b4c7795003d55f5c9255f
monokkai <egormayer08@mail.ru>
2025-10-05 23:02:20 +0200

    🐛fix: fixed folders and files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-import express from "express";
-
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+import "dotenv/config";
+import "./server.js";
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import express from "express";
+import app from "./app.js"
 
-const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send(200, "Hello, from /!");
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
