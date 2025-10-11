aa6bf16785e1eec9621b1b863ad169cbf3ba0096
monokkai <egormayer08@mail.ru>
2025-10-11 14:22:46 +0200

    ✨feat: updated config

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import { serial, varchar } from "drizzle-orm/mysql-core";
-import { pgTable } from "drizzle-orm/pg-core";
+import { pgTable, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
@@ -7,4 +7,6 @@ export const users = pgTable("users", {
   email: varchar("email", { length: 255 }).notNull(),
   password: varchar("password", { length: 255 }).notNull(),
   role: varchar("role", { length: 50 }).notNull().default("user"),
+  created_at: timestamp().defaultNow().notNull(),
+  updated_at: timestamp().defaultNow().notNull(),
 });
